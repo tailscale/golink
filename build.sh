3ec89abb08c98621867271700c3536995944b72e
Mihai Parparita <mihai@tailscale.com>
2022-07-29 11:34:50 -0700

    cmd: use absolute path for tool/ directory in Fly build.sh scripts
    Go 1.19 rejects relative paths in $PATH (https://go.dev/cl/403274),
    so resolve the tool/ path before adding it.
    
    Updates tailscale/tailscale#5210
    
    Change-Id: I891dd2739fe18534e0facb4dbbaaf6a98b91aeb1

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-export PATH="$(dirname "$0")/../../tool":$PATH
+export PATH=$(readlink -f $(dirname $0)/../../tool):$PATH
 
 go run github.com/tailscale/mkctr \
   --target="flyio" \

72851c233bdc9d4c27d4cf4c94a603084071744b
Mike O'Driscoll <mikeo@tailscale.com>
2025-04-22 13:19:44 -0400

    update flake vendor hash
    Signed-off-by: Mike O'Driscoll <mikeo@tailscale.com>

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
               "-X tailscale.com/version.longStamp=${tsVersion}"
               "-X tailscale.com/version.shortStamp=${tsVersion}"
             ];
-          vendorHash = "sha256-Nh/c6bHmaN9a1Ou2RYCMHqOsBtaVjLF27qnxeuCfx9Q="; # SHA based on vendoring go.mod
+          vendorHash = "sha256-kbr6LUI7wJPxn2OC5T9Il2krg91PeVDySh7axvl2kRQ="; # SHA based on vendoring go.mod
         };
       });
 

a98f4c4dffd89d9e2052904ac5abfa5b11016382
Jonathan Nobels <jnobels@gmail.com>
2025-05-29 15:24:04 -0400

    go.mod: update tailscale.com to 1.84.0 (#186)
    Signed-off-by: Jonathan Nobels <jonathan@tailscale.com>

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
               "-X tailscale.com/version.longStamp=${tsVersion}"
               "-X tailscale.com/version.shortStamp=${tsVersion}"
             ];
-          vendorHash = "sha256-RqKsIgwkCbIMQdCKtSHqW9eiZuNcZLCYeXFhPbgxjEU="; # SHA based on vendoring go.mod
+          vendorHash = "sha256-e9RXcIX4IxnVKk+fL0eBV1L3uChuUKlqZtsc7TdbPS0="; # SHA based on vendoring go.mod
         };
       });
 
